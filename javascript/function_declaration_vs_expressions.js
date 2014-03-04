f76e979143337fc4a30a168cf454fb345bf86e4c
daviskoh <koh.davis.0@gmail.com>
2014-03-03 23:49:17 -0500

    demonstration via console.log

--- javascript/function_declaration_vs_expressions.js
+++ javascript/function_declaration_vs_expressions.js
@@ -1,16 +1,19 @@
 // function declaration
 // function is defined when JS file is parsed
-function ello() {
-  return 'ello';
+console.log(ello1()); // => 'ello'
+function ello1() {
+  return 'ello1';
 };
 
 // function expression
 // function is defined when runtime reaches line of var declaration
-var ello = function() {
-  return 'ello';
+console.log(ello2()); // => undefined is not a function
+var ello2 = function() {
+  return 'ello2';
 };
 
 // same as above w/ useless named function
-var ello = function ello() {
-  return 'ello';
+console.log(ello3()); // => undefined is not a function
+var ello3 = function ello3() {
+  return 'ello3';
 };

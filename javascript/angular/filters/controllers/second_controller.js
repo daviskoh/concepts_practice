63c4e6f59e6ada7199b402fd07686e92571e93c3
daviskoh <koh.davis.0@gmail.com>
2014-04-20 13:35:13 -0400

    filtering and search

--- javascript/angular/filters/controllers/second_controller.js
+++ javascript/angular/filters/controllers/second_controller.js
@@ -1,3 +1,26 @@
 angular.module('myApp').controller('SecondCtrl', ['$scope', 'Data', function($scope, Data) {
     $scope.data = Data;
+
+    $scope.foods = [
+      {
+        name: 'chocolate',
+        taste: 5
+      },
+      {
+        name: 'milk',
+        taste: 3
+      },
+      {
+        name: 'bread',
+        taste: 1
+      },
+      {
+        name: 'icecream',
+        taste: 10
+      },
+      {
+        name: 'cheese',
+        taste: 5
+      }
+    ];
 }]);

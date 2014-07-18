a1d9f05f1ee02930826c8b06383f016a6a0f02b0
daviskoh <koh.davis.0@gmail.com>
2014-07-18 13:56:25 -0400

    basic spec

--- javascript/angular/generator-egghead/test/test-creation.js
+++ javascript/angular/generator-egghead/test/test-creation.js
@@ -3,34 +3,36 @@
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 
-describe('egghead generator', function () {
-  beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-      if (err) {
-        return done(err);
-      }
+describe('egghead generator', function() {
+    beforeEach(function(done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function(err) {
+            if (err) {
+                return done(err);
+            }
 
-      this.app = helpers.createGenerator('egghead:app', [
-        '../../app'
-      ]);
-      done();
-    }.bind(this));
-  });
-
-  it('creates expected files', function (done) {
-    var expected = [
-      // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
-    ];
-
-    helpers.mockPrompt(this.app, {
-      'someOption': true
+            this.app = helpers.createGenerator('egghead:app', [
+                '../../app'
+            ]);
+            done();
+        }.bind(this));
     });
-    this.app.options['skip-install'] = true;
-    this.app.run({}, function () {
-      helpers.assertFile(expected);
-      done();
+
+    it('creates expected files', function(done) {
+        var expected = [
+            // add files you expect to exist here.
+            'bower.json',
+            'package.json',
+            '.gitignore',
+            'Gruntfile.js'
+        ];
+
+        helpers.mockPrompt(this.app, {
+            'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function() {
+            helpers.assertFile(expected);
+            done();
+        });
     });
-  });
 });

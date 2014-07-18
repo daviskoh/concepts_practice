6ac0046115f4fdec34762cc8dfc840eb5ce39c69
daviskoh <koh.davis.0@gmail.com>
2014-07-18 15:15:53 -0400

    isolate scope &

--- javascript/angular/understanding_isolate_scope/src/app/controllers.js
+++ javascript/angular/understanding_isolate_scope/src/app/controllers.js
@@ -3,7 +3,9 @@
 angular.module('myControllers', [])
 
 .controller('FirstCtrl', function($scope) {
-  
+    $scope.logChore = function (chore) {
+        alert(chore + ' is done!');
+    };
 })
 
 .controller('SecondCtrl', function($scope) {
--- javascript/angular/understanding_isolate_scope/src/app/directives.js
+++ javascript/angular/understanding_isolate_scope/src/app/directives.js
@@ -1,3 +1,21 @@
 'use strict';
 
-angular.module('myDirectives', []);
+angular.module('myDirectives', [])
+
+.directive('kid', function () {
+    return {
+        restrict: 'E',
+        // breaks interaction w/ controllers that wrap around it
+            // breaks any bindings
+        scope: {
+            // take IN values into directive
+            // cant leak OUT
+            done: '&'
+        },
+        // create model chore available for use within directive
+        template: '<input type="text" ng-model="chore">' +
+        '{{chore}}' +
+        // done() is mapped to what is passed into done attribute on line 11 of index.html
+        '<button type="button" ng-click="done({chore: chore})">button</button>'
+    };
+});

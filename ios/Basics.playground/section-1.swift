91e69efff54fb089d4ecb282c57925d4538689ff
daviskoh <koh.davis.0@gmail.com>
2014-08-16 14:34:34 -0400

    swift

--- ios/Basics.playground/section-1.swift
+++ ios/Basics.playground/section-1.swift
@@ -2,4 +2,22 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+let 🐶🐮 = "dogcow"
+println(🐶🐮)
+
+// swift always chooses Double when inferring floating-point numbers
+let pi = 3.14159
+
+// combining integer & float will infer Double
+let anotherPi = 3 + 0.14159
+
+// can specify exponent in decmial numbers
+let bigThang = 1.25e2
+
+// numeric literals can be formatted w/ underscores
+let oneMillion = 1_000_000
+
+/*
+ * Numeric Type Conversion
+ */
+// T(v) converts value v to type T

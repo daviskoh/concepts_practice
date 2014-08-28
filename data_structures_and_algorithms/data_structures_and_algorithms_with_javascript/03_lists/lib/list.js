36eefea18ce35590a23c31f2e8cb9141d84cfca5
daviskoh <koh.davis.0@gmail.com>
2014-08-28 15:41:35 -0400

    init

---
+++ data_structures_and_algorithms/data_structures_and_algorithms_with_javascript/03_lists/lib/list.js
@@ -0,0 +1,54 @@
+// ADT - Abstract Data Type
+
+/**
+ * List ADT - ordered sequence of data
+ * each item stored called 'element'
+ * list w/ no items will be 'empty'
+ * 
+ * listSize (prop) - number of elements in list
+ * pos (prop) - current position in list
+ * length (prop) - returns number of elements in list
+ *
+ * clear (func) - clears all elements from list
+ * toString (func) - returns string representation of list
+ * getElement (func) - returns element at current position
+ * insert (func) - inserts new element after existing element
+ * append (func) - adds new element to end of list
+ * remove (func) - removes element from live
+ * front (func) - sets current position to 1st element of list
+ * end (func) - sets current position to last element of list
+ * prev (func) - moves current position back one element
+ * next (func) - moves current position forward one element
+ * currPos (func) - returns current position in list
+ * moveTo (func) - moves current position to specified position
+ * 
+ * 
+ * NOTE: List ADT does not specifiy storage function but array
+ * named dataStore will be used for following implementation
+ */
+
+// List constructor function
+module.exports = function List() {
+    // properties
+    this.listSize = 0;
+    this.pos = 0;
+    // initializes an empty array to store list elements
+    this.dataStore = [];
+
+    // methods
+    this.clear = clear;
+    this.find = find;
+    this.toString = toString;
+    this.insert = insert;
+    this.append = append;
+    this.remove = remove;
+    this.front = front;
+    this.end = end;
+    this.prev = prev;
+    this.next = next;
+    this.length = length;
+    this.currPos = currPos;
+    this.moveTo = moveTo;
+    this.getElement = getElement;
+    this.contains = contains;
+}
---
+++ data_structures_and_algorithms/data_structures_and_algorithms_with_javascript/03_lists/test/list.spec.js
@@ -0,0 +1,8 @@
+var List = require('../lib/list.js'),
+    should = require('should');
+
+describe('List', function() {
+    it('exists', function() {
+        console.log(should);
+    });
+});

03dd17c2506d11b04b20ba524fdfa3388e10bc51
daviskoh <koh.davis.0@gmail.com>
2014-08-28 17:38:00 -0400

    test append function

--- data_structures_and_algorithms/data_structures_and_algorithms_with_javascript/03_lists/test/list.spec.js
+++ data_structures_and_algorithms/data_structures_and_algorithms_with_javascript/03_lists/test/list.spec.js
@@ -9,4 +9,20 @@ describe('List', function() {
     it('can be instantiated', function() {
         (new List).should.be.ok;
     });
+
+    describe('List.append', function() {
+        var newList = new List;
+
+        it('allows an element to be appended to list', function() {
+            newList.append(1);
+            newList.listSize.should.eql(1);
+            newList.dataStore.should.containEql(1);
+        });
+
+        it('allows another element to be appended to list', function() {
+            newList.append(8);
+            newList.listSize.should.eql(2);
+            newList.dataStore.should.containEql(8);
+        });
+    });
 });

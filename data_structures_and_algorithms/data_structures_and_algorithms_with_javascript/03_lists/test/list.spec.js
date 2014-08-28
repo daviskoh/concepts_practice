27cd113eb6f3b68485e86cff6c0f223cf60876a8
daviskoh <koh.davis.0@gmail.com>
2014-08-28 17:30:04 -0400

    it can be instantiated

--- data_structures_and_algorithms/data_structures_and_algorithms_with_javascript/03_lists/test/list.spec.js
+++ data_structures_and_algorithms/data_structures_and_algorithms_with_javascript/03_lists/test/list.spec.js
@@ -5,4 +5,8 @@ describe('List', function() {
     it('exists', function() {
         List.should.be.ok;
     });
+
+    it('can be instantiated', function() {
+        (new List).should.be.ok;
+    });
 });

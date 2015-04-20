54b53400aa4ab8b8bae3f06f1f5928aa59510f97
daviskoh <koh.davis.0@gmail.com>
2015-04-19 23:26:41 -0400

    Update Models * tweak properties * add property isMale * add computed sex property

--- ios/swift/oop/simple-inheritance.swift
+++ ios/swift/oop/simple-inheritance.swift
@@ -1,27 +1,41 @@
 class Animal {
-    var name: String?
+    let isMale: Bool!
 
-    init(name: String?) {
-        self.name = name
+    var sex: String {
+        return isMale! ? "male" : "female"
     }
-    
+
+    init(isMale: Bool) {
+        self.isMale = isMale
+    }
+
     func walk() {
         println("walking")
     }
 }
 
 class Person: Animal {
+    var name: String!
+
+    init(isMale: Bool, name: String) {
+        super.init(isMale: isMale)
+
+        self.name = name
+    }
+
     func talk() {
         println("talking")
     }
 }
 
-let dog = Animal(name: nil)
+let dog = Animal(isMale: false)
 println("dog: \(dog)")
+println("dog's sex: \(dog.sex)")
 dog.walk()
 
-let person = Person(name: "bob")
+let person = Person(isMale: true, name: "bob")
 println("person: \(person)")
+println("person's name: \(person.name) & sex: \(person.sex)")
 person.walk()
 person.talk()
 

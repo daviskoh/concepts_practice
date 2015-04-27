8a8bfd4a730d71d6efa28d40ae846ea8d60534d0
Davis Koh <koh.davis.0@gmail.com>
2015-04-27 14:34:38 -0400

    add age in years

--- ios/swift/oop/simple-inheritance.swift
+++ ios/swift/oop/simple-inheritance.swift
@@ -8,6 +8,14 @@ class Animal {
         return isMale! ? "male" : "female"
     }
 
+    var age: Int {
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitYear, fromDate: self.dateOfBirth, toDate: NSDate(), options: nil)
+        let ageInYears = components.year
+
+        return ageInYears
+    }
+
     init(isMale: Bool, dateOfBirth: NSDate) {
         self.isMale = isMale
         self.dateOfBirth = dateOfBirth
@@ -32,6 +40,21 @@ class Person: Animal {
     }
 }
 
+class Dog: Animal {
+    var name: String!
+
+    var dogYears: Int {
+        return 1
+    }
+
+    init(isMale: Bool, dateOfBirth: NSDate, name: String) {
+        super.init(isMale: isMale, dateOfBirth: dateOfBirth)
+
+        self.name = name
+    }
+
+}
+
 let dog = Animal(isMale: false, dateOfBirth: NSDate())
 println("dog: \(dog)")
 println("dog's sex: \(dog.sex)")
@@ -42,6 +65,7 @@ let person = Person(isMale: true, dateOfBirth: NSDate(), name: "bob")
 println("person: \(person)")
 println("person's name: \(person.name) & sex: \(person.sex)")
 println("person's DOB: \(person.dateOfBirth)")
+println("person's age in years: \(person.age)")
 person.walk()
 person.talk()
 

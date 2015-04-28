99cfd7c9a6e11977e31d30e80510caf121985eae
Davis Koh <koh.davis.0@gmail.com>
2015-04-28 17:31:28 -0400

    add member values & add ex consuming them

--- ios/swift/types/enums.swift
+++ ios/swift/types/enums.swift
@@ -1,3 +1,20 @@
 enum CardinalDirection {
+    // member values
+    case North, East, South, West
+}
 
+var direction = CardinalDirection.North
+// once direction is declared as type CardinalDirection...
+direction = .South
+
+// matching member values
+switch direction {
+case .North:
+    println("heading north")
+case .South:
+    println("heading south")
+case .East:
+    println("heading east")
+case .West:
+    println("heading west")
 }

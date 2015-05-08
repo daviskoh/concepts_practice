30c3c40eb6e828ac675e22534d3db1138a706d67
Davis Koh <koh.davis.0@gmail.com>
2015-05-08 13:00:58 -0400

    init project

---
+++ ObjectiveC/ProgrammaticViews/ProgrammaticViews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProgrammaticViews
+//
+//  Created by Davis Koh on 5/8/15.
+//  Copyright (c) 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectiveC/ProgrammaticViews/ProgrammaticViews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProgrammaticViews
+//
+//  Created by Davis Koh on 5/8/15.
+//  Copyright (c) 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

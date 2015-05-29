c59ada873bf02c1b34932ac2d6bac23d15347ed1
Davis Koh <koh.davis.0@gmail.com>
2015-05-29 14:32:20 -0400

    add method

--- javascript/es6/try-babel/index.es6.js
+++ javascript/es6/try-babel/index.es6.js
@@ -1,3 +1,6 @@
 class Person {
-
+    talk() {
+        console.log('hello');
+    }
 }
+
--- javascript/es6/try-babel/index.js
+++ javascript/es6/try-babel/index.js
@@ -1,7 +1,20 @@
-"use strict";
+'use strict';
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var Person = function Person() {
-  _classCallCheck(this, Person);
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var Person = (function () {
+    function Person() {
+        _classCallCheck(this, Person);
+    }
+
+    _createClass(Person, [{
+        key: 'talk',
+        value: function talk() {
+            console.log('hello');
+        }
+    }]);
+
+    return Person;
+})();

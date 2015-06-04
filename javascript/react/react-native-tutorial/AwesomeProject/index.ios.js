4b1bf771f743bfdbf5fd582be909c02f583f49ac
daviskoh <koh.davis.0@gmail.com>
2015-06-04 17:42:38 -0400

    readd mock data

--- javascript/react/react-native-tutorial/AwesomeProject/index.ios.js
+++ javascript/react/react-native-tutorial/AwesomeProject/index.ios.js
@@ -7,25 +7,32 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var MOCKED_MOVIES_DATA = [{
+    title: 'Title',
+    year: '2015',
+    posters: { thumbnail: 'http://i.imgur.com/UePbdph.jpg' }
+  }];
+
 var AwesomeProject = React.createClass({
   render: function() {
+    var movie = MOCKED_MOVIES_DATA[0];
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
     );
   }
@@ -38,15 +45,9 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  thumbnail: {
+    width: 53,
+    height: 81,
   },
 });
 

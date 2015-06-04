c85894020702819a34b7d81e360caa25164280ad
daviskoh <koh.davis.0@gmail.com>
2015-06-04 17:35:14 -0400

    revert

--- javascript/react/react-native-tutorial/AwesomeProject/index.ios.js
+++ javascript/react/react-native-tutorial/AwesomeProject/index.ios.js
@@ -6,63 +6,48 @@
 
 var React = require('react-native');
 var {
-    AppRegistry,
-    Image,
-    StyleSheet,
-    Text,
-    View,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
 } = React;
 
-var MOCKED_MOVIES_DATA = [{
-    title: 'Title',
-    year: '2015',
-    posters: {
-        thumbnail: 'http://i.imgur.com/UePbdph.jpg',
-    },
-}];
-
 var AwesomeProject = React.createClass({
-    render: function () {
-        var movie = MOCKED_MOVIES_DATA[0];
-
-        return ( < View style = {
-                styles.container
-            } >
-            < Image source = {
-                {
-                    uri: movie.posters.thumbnail
-                }
-            }
-            style = {
-                styles.thumbnail
-            }
-            /> < View style = {
-                styles.rightContainer
-            } >
-            < Text style = {
-                styles.title
-            } > {
-                movie.title
-            } < /Text> < Text style = {
-                styles.year
-            } > {
-                movie.year
-            } < /Text> < /View> < /View>
-        );
-    }
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
 });
 
 var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    thumbnail: {
-        width: 53,
-        height: 81,
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);

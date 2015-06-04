b92dc95ce6badc8e8bbd913ec595de5b3c47df0f
daviskoh <koh.davis.0@gmail.com>
2015-06-04 17:32:01 -0400

    beautify index.ios.js

--- javascript/react/react-native-tutorial/AwesomeProject/index.ios.js
+++ javascript/react/react-native-tutorial/AwesomeProject/index.ios.js
@@ -6,46 +6,63 @@
 
 var React = require('react-native');
 var {
-  AppRegistry,
-  Image,
-  StyleSheet,
-  Text,
-  View,
+    AppRegistry,
+    Image,
+    StyleSheet,
+    Text,
+    View,
 } = React;
 
-var MOCKED_MOVIES_DATA = [
-    {
-        title: 'Title',
-        year: '2015',
-        posters: { thumbnail: 'http://i.imgur.com/UePbdph.jpg', },
-    }
-];
+var MOCKED_MOVIES_DATA = [{
+    title: 'Title',
+    year: '2015',
+    posters: {
+        thumbnail: 'http://i.imgur.com/UePbdph.jpg',
+    },
+}];
 
 var AwesomeProject = React.createClass({
-  render: function() {
-    var movie = MOCKED_MOVIES_DATA[0];
- 
-    return (
-      <View style={styles.container}>
-        <Text>{movie.title}</Text>
-        <Text>{movie.year}</Text>
-        <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail}/>
-      </View>
-    );
-  }
+    render: function () {
+        var movie = MOCKED_MOVIES_DATA[0];
+
+        return ( < View style = {
+                styles.container
+            } >
+            < Image source = {
+                {
+                    uri: movie.posters.thumbnail
+                }
+            }
+            style = {
+                styles.thumbnail
+            }
+            /> < View style = {
+                styles.rightContainer
+            } >
+            < Text style = {
+                styles.title
+            } > {
+                movie.title
+            } < /Text> < Text style = {
+                styles.year
+            } > {
+                movie.year
+            } < /Text> < /View> < /View>
+        );
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    thumbnail: {
+        width: 53,
+        height: 81,
+    }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);

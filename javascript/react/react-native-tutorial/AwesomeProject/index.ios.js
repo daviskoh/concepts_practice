2773423e3ece627e576d2caa96a86f6e9de2578f
daviskoh <koh.davis.0@gmail.com>
2015-06-04 17:45:55 -0400

    position image and text horizontally

--- javascript/react/react-native-tutorial/AwesomeProject/index.ios.js
+++ javascript/react/react-native-tutorial/AwesomeProject/index.ios.js
@@ -41,13 +41,25 @@ var AwesomeProject = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  rightContainer: {
+    flex: 1,
+  },
   thumbnail: {
     width: 53,
     height: 81,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
   },
 });
 

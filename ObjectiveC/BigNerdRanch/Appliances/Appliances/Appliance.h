be74cb097aeff1fbb3294d777dced49827b0c64f
daviskoh <koh.davis.0@gmail.com>
2015-07-13 22:51:18 -0400

    add Lifetime Specifiers guide

--- ObjectiveC/BigNerdRanch/Appliances/Appliances/Appliance.h
+++ ObjectiveC/BigNerdRanch/Appliances/Appliances/Appliance.h
@@ -8,6 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ * Property Guidelines for Lifetime Specifiers:
+ * Types: unsafe_unretained (default), strong, weak, copy
+ *
+ * unsafe_unretained - non-objects
+ * strong - objects
+ * weak - not imply ownership of object pointed to
+ * copy - objects that have mutable subclasses (think passing NSMutableString then mutating it elsewhere accidentally changing the property)
+ */
+
 @interface Appliance : NSObject
 
 @property (copy) NSString *productName;

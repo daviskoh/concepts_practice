b6468e25b1ced286117086306a582db25b6c030c
daviskoh <koh.davis.0@gmail.com>
2015-07-13 23:26:00 -0400

    add blocks ex

---
+++ ObjectiveC/BigNerdRanch/VowelMovement/VowelMovement/main.m
@@ -0,0 +1,49 @@
+//
+//  main.m
+//  VowelMovement
+//
+//  Created by Davis Koh on 7/13/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSArray *oldStrings = @[@"Sauerkraut", @"Raygun", @"Big Nerd Ranch", @"Mississippi"];
+        NSLog(@"old strings: %@", oldStrings);
+        
+        NSMutableArray *newStrings = [NSMutableArray array];
+        
+        NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
+        
+        void (^devowelizer)(id, NSUInteger, BOOL *);
+        
+        devowelizer = ^(id string, NSUInteger i, BOOL *stop) {
+            NSRange yRange = [string rangeOfString:@"y"
+                                           options:NSCaseInsensitiveSearch];
+            
+            if (yRange.location != NSNotFound) {
+                *stop = YES;
+                return;
+            }
+            
+            NSMutableString *newString = [NSMutableString stringWithString:string];
+            
+            for (NSString *s in vowels) {
+                NSRange fullRange = NSMakeRange(0, newString.length);
+                
+                [newString replaceOccurrencesOfString:s
+                                           withString:@""
+                                              options:NSCaseInsensitiveSearch
+                                                range:fullRange];
+                
+                [newStrings addObject:newString];
+            }
+        };
+        
+        [oldStrings enumerateObjectsUsingBlock:devowelizer];
+        NSLog(@"new strings: %@", newStrings);
+    }
+    return 0;
+}

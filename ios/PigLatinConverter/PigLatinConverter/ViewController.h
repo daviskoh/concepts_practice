80c43916a2a883b783a13cfb4dcc62c3761ffde1
daviskoh <koh.davis.0@gmail.com>
2015-07-28 21:59:54 -0400

    init pig latin

---
+++ ios/PigLatinConverter/PigLatinConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PigLatinConverter
+//
+//  Created by Davis Koh on 7/28/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios/PigLatinConverter/README.md
@@ -0,0 +1,9 @@
+# Pig Latin Translator
+
+This project takes a word and translates it into Pig Latin
+
+Practice:  
+
+- Programmatic UI
+- Programmatic Auto Layout
+

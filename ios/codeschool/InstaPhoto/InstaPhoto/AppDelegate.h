f44d1892ab9cf7fb526aa7c2b892e3fb6abe9215
daviskoh <koh.davis.0@gmail.com>
2015-08-26 19:39:04 -0400

    init InstaPhoto

---
+++ ios/codeschool/InstaPhoto/InstaPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaPhoto
+//
+//  Created by Davis Koh on 8/26/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

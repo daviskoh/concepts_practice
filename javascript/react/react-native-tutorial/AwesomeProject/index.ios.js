dd0a3a41d481c8c8f8c1f3fa8ecdc9a97d27ffbf
daviskoh <koh.davis.0@gmail.com>
2015-09-10 20:12:48 -0400

    remove unneeded mock movie data

--- javascript/react/react-native-tutorial/AwesomeProject/index.ios.js
+++ javascript/react/react-native-tutorial/AwesomeProject/index.ios.js
@@ -17,14 +17,6 @@ var {
   ListView,
 } = React;
 
-var MOCKED_MOVIES_DATA  = [{ 
-    title: 'Title',
-    year: '2015',
-    posters: {
-        thumbnail: 'http://i.imgur.com/UePbdph.jpg'
-    }
-}];
-
 var REQUEST_URL = [
     'https:/',
     'raw.githubusercontent.com',

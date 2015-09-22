8b002ff4599f9ea9a7e1d1af59401be64814e975
daviskoh <koh.davis.0@gmail.com>
2015-09-21 21:28:15 -0400

    re init

---
+++ ios/codeschool/InstaPhotoSwift/InstaPhotoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaPhotoSwift
+//
+//  Created by Davis Koh on 9/21/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

3e9265c2bc5c0c64415f20c8fcb3012a2f3643f1
daviskoh <koh.davis.0@gmail.com>
2015-11-17 15:11:58 -0500

    add trivial ex of middleware logic

--- javascript/koa/basic-ex/app.js
+++ javascript/koa/basic-ex/app.js
@@ -1,11 +1,28 @@
 const Koa = require('koa');
+const co = require('co');
 const app = new Koa();
 
 const port = process.env.PORT || 3000;
 
-app.use(ctx => {
+app.use(co.wrap(function *(ctx, next) {
+    const start = new Date;
+    yield next();
+    const ms = new Date - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+}));
+
+app.use(co.wrap(function *(ctx, next) {
+    console.log('sending res');
     ctx.body = 'hello world';
-});
+    yield next();
+    console.log('res sent');
+}));
+
+app.use(co.wrap(function *(ctx, next) {
+    console.log('doing nothing');
+    ctx.body += '***';
+    yield next();
+}));
 
 console.log(`listening on port: ${port}`);
 app.listen(3000);

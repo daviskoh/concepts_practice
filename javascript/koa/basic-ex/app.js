0cf8c3eb856e4ea1a9c930ae15019a06558c4150
daviskoh <koh.davis.0@gmail.com>
2015-11-17 14:48:09 -0500

    opt for using node v4.2.2

--- javascript/koa/basic-ex/app.js
+++ javascript/koa/basic-ex/app.js
@@ -1,8 +1,12 @@
 const Koa = require('koa');
 const app = new Koa();
 
+const port = process.env.PORT || 3000;
+
 app.use(ctx => {
     ctx.body = 'hello world';
 });
 
+console.log(`listening on port: ${port}`);
 app.listen(3000);
+

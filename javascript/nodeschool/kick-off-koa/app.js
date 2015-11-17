2d9c52f720c0425fb679ea215556d99728f6788d
daviskoh <koh.davis.0@gmail.com>
2015-11-17 13:45:15 -0500

    challenge 2

--- javascript/nodeschool/kick-off-koa/app.js
+++ javascript/nodeschool/kick-off-koa/app.js
@@ -5,9 +5,21 @@ const app = koa();
 
 const port = process.argv[2];
 
-app.use(function *() {
+app.use(function *(next) {
     // this - encapsulates req & res objcs into single obj
+
+    if (this.path !== '/') return yield next;
     this.body = 'hello koa';
+});
+
+app.use(function *(next) {
+    if (this.path !== '/404') return yield next;
+    this.body = 'page not found';
+});
+
+app.use(function *(next) {
+    if (this.path !== '/500') return yield next;
+    this.body = 'internal server error';
 });
 
 app.listen(port);

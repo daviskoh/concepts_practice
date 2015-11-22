898eeb5bf58cdbffcd82c5304036fbacce44fece
daviskoh <koh.davis.0@gmail.com>
2015-11-21 22:55:39 -0500

    init boxing timer

---
+++ ios/BoxingTimer/BoxingTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BoxingTimer
+//
+//  Created by Davis Koh on 11/21/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/BoxingTimer/BoxingTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BoxingTimer
+//
+//  Created by Davis Koh on 11/21/15.
+//  Copyright © 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

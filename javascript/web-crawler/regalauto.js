0f7a727da52062c20d9d7b642636ab19b1ac148f
daviskoh <koh.davis.0@gmail.com>
2015-12-01 18:03:27 -0500

    attempt to extract iframes from regalauto

---
+++ javascript/web-crawler/regalauto.js
@@ -0,0 +1,55 @@
+var EightyApp = function() {
+    this.processDocument = function(html, url, headers, status, jQuery) {
+            var app = this;
+            var $ = jQuery;
+            var $html = app.parseHtml(html, $);
+            var object = {};
+
+            object.title = $html.filter('title').text();
+
+            var iframes = [];
+            $html.find('iframe').each(function (i, elem) {
+                iframes.push({
+                  src: $(elem).attr('src')
+                });
+            });
+
+            object.iframes = iframes;
+
+            object.iframesCount = $html.find('iframe').length;
+
+            return JSON.stringify(object);
+    };
+
+    this.parseLinks = function(html, url, headers, status, jQuery) {
+        var app = this;
+        var $ = jQuery;
+        var $html = app.parseHtml(html, $);
+        var links = [];
+
+        // gets all links in the html document
+        $html.find('a').each(function(i, obj) {
+            var link = app.makeLink(url, $(this).attr('href'));
+            if (link !== null) {
+                var eightyvalue = app.get80Value(url);
+                if (eightyvalue === null) eightyvalue = url;
+                link = app.append80FlagToLink(eightyvalue, link);
+                links.push(link);
+            }
+        });
+
+        return links;
+    };
+}
+
+try {
+    // Testing
+    module.exports = function(EightyAppBase) {
+            EightyApp.prototype = new EightyAppBase();
+            return new EightyApp();
+    }
+} catch(e) {
+    // Production
+    console.log("Eighty app exists.");
+    EightyApp.prototype = new EightyAppBase();
+}

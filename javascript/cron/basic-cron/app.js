46bac5e15d1f63c25fd85a97a3ba9ddef0a8f1b0
daviskoh <koh.davis.0@gmail.com>
2015-12-10 14:46:02 -0500

    remove job stopping logic

--- javascript/cron/basic-cron/app.js
+++ javascript/cron/basic-cron/app.js
@@ -5,8 +5,6 @@ const CronJob = require('cron').CronJob;
 var count = 0;
 
 const job = new CronJob('*/5 * * * * *', () => {
-  if (count === 5) return job.stop();
-
   console.log('meow');
   count++;
 });

5d4ad27c9ea9ac8e19457101638fc8b1c3516358
daviskoh <koh.davis.0@gmail.com>
2015-12-14 14:57:53 -0500

    add console.log when data available

--- mongo/write-stream/app.js
+++ mongo/write-stream/app.js
@@ -13,12 +13,17 @@ const streamToMongo = require('stream-to-mongo')(options);
 
 // read / write json contents to mongo
 const fileName = 'scrape.json';
+const parseStream = JSONStream.parse(['pages']);
 const readStream = fs.createReadStream(fileName, {encoding:'utf-8'})
-  .pipe(JSONStream.parse(['pages']))
+  .pipe(parseStream)
   .pipe(streamToMongo);
 
 readStream.on('error', () => {
   throw new Error('error reading file');
+});
+
+parseStream.on('data', (data) => {
+  console.log(data);
 });
 
 streamToMongo.on('finish', () => {

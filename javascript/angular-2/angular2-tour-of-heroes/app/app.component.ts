218d08c3f637b2d0a6e0a0222c3caf8ff14e01af
daviskoh <koh.davis.0@gmail.com>
2015-12-16 16:10:53 -0500

    add list of heroes and some styles

--- javascript/angular-2/angular2-tour-of-heroes/app/app.component.ts
+++ javascript/angular-2/angular2-tour-of-heroes/app/app.component.ts
@@ -13,6 +13,17 @@ interface Hero {
     <h1>
       {{title}}
     </h1>
+
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes">
+        <span class="badge">
+          {{hero.id}}
+        </span>
+        {{hero.name}}
+      </li>
+    </ul>
+
     <h2>
       {{hero.name}} details!
     </h2>
@@ -22,10 +33,33 @@ interface Hero {
     </div>
     <div>
       <label>name: </label>
-      <input [(ngModel)]="hero.name"
-             placeholder="name">
+      <div>
+        <input [(ngModel)]="hero.name"
+               placeholder="name">
+      </div>
     </div>
-  `
+  `,
+  // NOTE: how styles are scoped to component
+  styles: [`
+    .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+
+    .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+
+    .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+
+    .heroes .badge {
+      font-size: small;
+      color: white;
+      padding: 0.1em 0.7em;
+      background-color: #369;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -1px;
+    }
+
+    .selected { background-color: #EEE; color: #369; }
+  `]
 })
 
 export class AppComponent {
@@ -33,6 +67,19 @@ export class AppComponent {
   public hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  }
+  };
+  public heroes = HEROES;
 }
 
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];

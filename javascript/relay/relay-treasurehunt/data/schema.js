4e4d071a75ad99ace169af21d4e6bab715d7db72
daviskoh <koh.davis.0@gmail.com>
2015-12-16 17:49:44 -0500

    mappings and obj types

--- javascript/relay/relay-treasurehunt/data/schema.js
+++ javascript/relay/relay-treasurehunt/data/schema.js
@@ -31,36 +31,32 @@ import {
 
 import {
   // Import methods that your schema can use to interact with your database
-  User,
-  Widget,
-  getUser,
-  getViewer,
-  getWidget,
-  getWidgets,
+  Game,
+  HidingSpot,
+  checkHidingSpotForTreasure,
+  getGame,
+  getHidingSpot,
+  getHidingSpots,
+  getTurnsRemaining,
 } from './database';
 
-/**
- * We get the node interface and field from the Relay library.
- *
- * The first method defines the way we resolve an ID to its object.
- * The second defines the way we resolve an object to its GraphQL type.
- */
-var {nodeInterface, nodeField} = nodeDefinitions(
-  (globalId) => {
-    var {type, id} = fromGlobalId(globalId);
-    if (type === 'User') {
-      return getUser(id);
-    } else if (type === 'Widget') {
-      return getWidget(id);
+// map from objs to GraphQL types on line 66
+const {nodeInterface, nodeField} = nodeDefinitions(
+  (global) => {
+    const {type, id} = fromGlobalId(globalId);
+    if (type === 'Game') {
+      return getGame(id);
+    } else if (type === 'HidingSpot') {
+      return getHidingSpot(id);
     } else {
       return null;
     }
   },
   (obj) => {
-    if (obj instanceof User) {
-      return userType;
-    } else if (obj instanceof Widget)  {
-      return widgetType;
+    if (obj instanceof Game) {
+      return gameType;
+    } else if (obj instanceof HidingSpot) {
+      return hidingSpotType;
     } else {
       return null;
     }
@@ -68,55 +64,68 @@ var {nodeInterface, nodeField} = nodeDefinitions(
 );
 
 /**
- * Define your own types here
- */
+  * GraphQL Types
+  */
 
-var userType = new GraphQLObjectType({
-  name: 'User',
-  description: 'A person who uses our app',
+const gameType = new GraphQLObjectType({
+  name: 'Game',
+  description: 'A treasure search game',
   fields: () => ({
-    id: globalIdField('User'),
-    widgets: {
-      type: widgetConnection,
-      description: 'A person\'s collection of widgets',
+    id: globalIdField('Game'),
+    hidingSpots: {
+      type: hidingSpotConnection,
+      description: 'Places where treasure might be hidden',
       args: connectionArgs,
-      resolve: (_, args) => connectionFromArray(getWidgets(), args),
+      resolve: (game, args) => connectionFromArray(getHidingSpots(), args),
+    },
+    turnsRemaining: {
+      type: GraphQLInt,
+      description: 'The number of turns a player has left to find the treasure',
+      resolve: () => getTurnsRemaining(),
     },
   }),
   interfaces: [nodeInterface],
 });
 
-var widgetType = new GraphQLObjectType({
-  name: 'Widget',
-  description: 'A shiny widget',
+const hidingSpotType = new GraphQLObjectType({
+  name: 'HidingSpot',
+  description: 'A place where you might find treasure',
   fields: () => ({
-    id: globalIdField('Widget'),
-    name: {
-      type: GraphQLString,
-      description: 'The name of the widget',
+    id: globalIdField('HidingSpot'),
+    hasBeenChecked: {
+      type: GraphQLBoolean,
+      description: 'True if this spot has already been checked for treasure',
+      resolve: (hidingSpot) => hidingSpot.hasBeenChecked,
+    },
+    hasTreasure: {
+      type: GraphQLBoolean,
+      description: 'True if this hiding spot holds treasure',
+      resolve: (hidingSpot) => {
+        if (hidingSpot.hasBeenChecked) {
+          return hidingSpot.hasTreasure;
+        } else {
+          return null;
+        }
+      },
     },
   }),
   interfaces: [nodeInterface],
 });
 
-/**
- * Define your own connection types here
- */
-var {connectionType: widgetConnection} =
-  connectionDefinitions({name: 'Widget', nodeType: widgetType});
+// game has many hiding spots
+// create connection used to link them together
+// FIXME: below NOT semantically clear as to what its doing
+const {connectionType: hidingSpotConnection} =
+  connectionDefinitions({name: 'HidingSpot', nodeType: hidingSpotType});
 
-/**
- * This is the type that will be the root of our query,
- * and the entry point into our schema.
- */
 var queryType = new GraphQLObjectType({
   name: 'Query',
   fields: () => ({
     node: nodeField,
     // Add your own root fields here
     viewer: {
-      type: userType,
-      resolve: () => getViewer(),
+      type: gameType,
+      resolve: () => getGame(),
     },
   }),
 });

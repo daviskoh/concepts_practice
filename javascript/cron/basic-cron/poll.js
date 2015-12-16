73c1ea60e1e04743364196cdeb6ca2e43c0eb118
daviskoh <koh.davis.0@gmail.com>
2015-12-16 10:52:51 -0500

    add crude ex of how polling can work

---
+++ javascript/cron/basic-cron/poll.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const fs = require('fs');
+
+// after 10 sec, create a file
+// start polling immediately, and stop when found
+
+setTimeout(() => {
+  fs.writeFileSync('data.json', '{"bro": "foo"}', 'utf8');
+  console.log('-- wrote to data.json --');
+}, 10000);
+
+var interval = setInterval(() => {
+  console.log('polling...');
+
+  var data = fs.readFileSync('data.json', 'utf8');
+  if (data) {
+    console.log(data);
+    fs.writeFileSync('data.json', '', 'utf8');
+    return clearInterval(interval);
+  }
+}, 2000);
+

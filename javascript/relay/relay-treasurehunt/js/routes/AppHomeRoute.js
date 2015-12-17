815dc572df90ba3dd9197ea175049007613bce6a
daviskoh <koh.davis.0@gmail.com>
2015-12-17 11:52:18 -0500

    update component & routes

--- javascript/relay/relay-treasurehunt/js/routes/AppHomeRoute.js
+++ javascript/relay/relay-treasurehunt/js/routes/AppHomeRoute.js
@@ -4,9 +4,10 @@ export default class extends Relay.Route {
   static queries = {
     viewer: () => Relay.QL`
       query {
-        viewer
+        game
       }
     `,
   };
   static routeName = 'AppHomeRoute';
 }
+

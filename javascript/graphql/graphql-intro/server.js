ba595dc1a4447083ff5b13de604a2b3abc04c8f4
daviskoh <koh.davis.0@gmail.com>
2015-12-17 16:28:16 -0500

    add & consume schema

--- javascript/graphql/graphql-intro/server.js
+++ javascript/graphql/graphql-intro/server.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import schema from './schema';
+import { graphql } from 'graphql';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.text({
+  type: 'application/graphql'
+}));
+
 app.post('/graphql', (req, res) => {
-  res.send('Hello!');
+  // TODO: use async/await
+  graphql(schema, req.body)
+    .then((result) => {
+      res.send(JSON.stringify(result, null, 2));
+    });
 });
 
 const server = app.listen(PORT, () => {
